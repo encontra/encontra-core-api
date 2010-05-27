bfa6cc31168e96d799dac8521317c3aac8347352
Ricardo Dias <rspd.sao.pedro@gmail.com>
2010-05-27 15:00:35 +0100

    + Add: Main Entities of the EnContRA CORE API. ~Some Elements are not filled in, just here for conceptualization.

---
+++ src/main/java/pt/inevo/encontra/query/AproximateKnnQuery.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pt.inevo.encontra.query;
+
+/**
+ * Approximate K Nearest Neighbour Query - uses stop conditions to accelerate
+ * the Knn Query
+ * @author ricardo
+ */
+public class AproximateKnnQuery extends KnnQuery {
+
+}
