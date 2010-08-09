6245b5430e3e3068fff0efbe40f404de8b25b8c1
Ricardo Dias <rspd.sao.pedro@gmail.com>
2010-08-09 13:28:11 +0100

    Added to more methods to the EntryProvider. Support for the EntryProvider in the LuceneIndex (needs some tests).

--- src/main/java/pt/inevo/encontra/index/EntryProvider.java
+++ src/main/java/pt/inevo/encontra/index/EntryProvider.java
@@ -1,21 +1,23 @@
 package pt.inevo.encontra.index;
 
+import java.io.Serializable;
 import java.util.List;
 
 public interface EntryProvider<E>{
 
     public void begin();
     public void end();
+    public boolean setCursor(E entry);
     
     public E getFirst();
     public E getLast();
+    public E getEntry(Serializable key);
+    public List<E> getAll();
+    public int size();
 
     public E getNext();
     public boolean hasNext();
     public E getPrevious();
     public boolean hasPrevious();
     public boolean contains(E entry);
-
-    public List<E> getAll();
-    public int size();
 }
