10e33df8a1cdd73d82222a081f8103e1801c4077
jpvguerreiro <jpvguerreiro@gmail.com>
2015-02-06 18:31:43 +0000

    Bug fix: Jenkins Build fail

--- src/main/java/pt/inevo/encontra/index/AbstractIndex.java
+++ src/main/java/pt/inevo/encontra/index/AbstractIndex.java
@@ -2,6 +2,8 @@ package pt.inevo.encontra.index;
 
 import pt.inevo.encontra.storage.IEntry;
 
+import java.io.IOException;
+
 /**
  * Generic index structure.
  * Defines the shared methods by all the index structures that implement this
@@ -19,4 +21,6 @@ public abstract class AbstractIndex<E extends IEntry> implements Index<E> {
     public void setEntryFactory(IndexEntryFactory entryFactory) {
         this.entryFactory = entryFactory;
     }
+
+    public abstract void close() throws IOException;
 }
